cf871a81de005b9e2b11eb96239c038a960678ca
Suraj Kumar <57040963+suraj911kr@users.noreply.github.com>
2023-06-26 14:41:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-import caeser_dict
-import morse_dict
 
-def caeser(phrase):
-    code = ""
-    for letter in phrase:
-        code = code + caeser_dict.cce[letter]
-    return code
+alphaArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+              'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+              's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def caeser_decrypt(phrase):
-    code = ""
-    for letter in phrase:
-        code = code + caeser_dict.ccd[letter]
-    return code
+caeserDict = {}
 
-def morse(phrase):
-    code = ""
-    for letter in phrase:
-        code = code + morse_dict.me[letter]
-    return code
+def caeserEncryption(message, shift):
+    for i in range(len(alphaArray)):
+        key = alphaArray[i]
+        value_index = (i+shift) % len(alphaArray)
+        value = alphaArray[value_index]
+        caeserDict[key] = value
 
-def morse_decrypt(phrase):
-    code = ""
-    for letter in phrase:
-        code = code + morse_dict.md[letter]
-    return code
+    encryptedMsg = ""
+    for letter in message:
+        if letter==' ':
+            encryptedMsg = encryptedMsg + letter
+        else:
+            encryptedMsg = encryptedMsg + caeserDict[letter]
+    return encryptedMsg
 
-x = str(input("Enter Command: "))
+def caeserDecryption(message, shift):
+    for i in range(len(alphaArray)):
+        key = alphaArray[i]
+        value_index = (i - shift) % len(alphaArray)
+        value = alphaArray[value_index]
+        caeserDict[key] = value
 
-if x=="Encryption":
-    y = str(input("Encryption Method: "))
-    z = str(input("Enter message: "))
-    if y=="Caeser Cipher":
-        print(caeser(z))
-    elif y=="Morse":
-        print(morse(z))
-    else:
-        print("Invalid encryption method")
-elif x=="Decryption":
-    v = str(input("Decryption Method: "))
-    w = str(input("Enter message: "))
-    if v=="Caeser Cipher":
-        print(caeser_decrypt(w))
-    elif v=="Morse":
-        print(morse_decrypt(w))
-    else:
-        print("Invalid decryption method")
+    decryptedMsg = ""
+    for letter in message:
+        if letter==' ':
+            decryptedMsg = decryptedMsg + letter
+        else:
+            decryptedMsg = decryptedMsg + caeserDict[letter]
+    return decryptedMsg
+
+command = str(input("Enter Command: "))
+s = int(input("Shift value: "))
+
+if command == "Encryption":
+    msg = str(input("Enter message for Encryption: "))
+    print(caeserEncryption(msg, s))
+
+elif command == "Decryption":
+    msg = str(input("Enter message for Decryption: "))
+    print(caeserDecryption(msg, s))
 
 else:
-    print("Invalid Command")
+    print("Invalid command.")
+
